bac1b99248ab6f2f51e709b62ffe3001b157d5ac
lepetoc <lepetoctgp@outlook.fr>
2023-12-04 18:49:14 +0100

    Connection to mariadb database completed

--- controllers/buildController.js
+++ controllers/buildController.js
@@ -1,10 +1,30 @@
+require('dotenv').config()
+
 const mariadb = require('mariadb');
-const pool = mariadb.createPool({ host: "localhost", user: "admin", password: "admin", database: "eldenring", connectionLimit: 5 });
+const pool = mariadb.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  connectionLimit: 5
+});
 
 // controllers/buildController.js
-const getAllBuilds = (req, res) => {
-  // Logique pour récupérer tous les builds
-  res.send('Liste de tous les builds');
+const getAllBuilds = async (req, res) => {
+  let conn;
+    try {
+        conn = await pool.getConnection();
+        const response = await conn.query(
+            "SELECT * FROM builds",
+        );
+        //console.log(response);
+        res.json({ response });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred");
+    } finally {
+        if (conn) conn.release();
+    }
 };
 
 const getBuildById = (req, res) => {
@@ -19,19 +39,38 @@ const getBuildsFromUser = (req, res) => {
   res.send(`Build de l'utilisateur ${buildId}`);
 };
 
-const createBuild = (req, res) => async () => {
+const createBuild = async (req, res) => {
+  const { userId, name, description, itemsArray, tagsArray, visibility } = req.body;
+  let currentDate = new Date();
   let conn;
   try {
     conn = await pool.getConnection();
-    const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-    res.send('Build créé');
-    console.log(res);
-  } catch (err) {
-    throw err;
+    const response = await conn.query(
+      "INSERT INTO builds (user, name, description, items, version, tags, visibility, creationDate, modificationDate, creationUser, modificationUser, active) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        userId, // Remplacez par l'ID de l'utilisateur correspondant
+        name, // Nom du build
+        description, // Description du build
+        JSON.stringify(itemsArray), // Les items sous forme de chaîne JSON
+        "Version du jeu", // Version du jeu
+        JSON.stringify(tagsArray), // Les tags sous forme de chaîne JSON
+        visibility, // Visibilité (true pour public, false pour privé)
+        currentDate, // Date de création
+        currentDate, // Date de modification
+        "Nom de l'utilisateur créateur", // Utilisateur qui a créé le build
+        "Nom de l'utilisateur modificateur", // Utilisateur qui a modifié le build
+        true // Active (true ou false)
+      ]
+    );
+
+    console.log(response);
+    res.send("Build created");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred");
   } finally {
-    if (conn) return conn.end();
+    if (conn) conn.release();
   }
-  // Logique pour créer un build
 };
 
 const updateBuild = (req, res) => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,29 @@
-const mariadb = require('mariadb');
-const pool = mariadb.createPool({ host: "localhost", user: "admin", password: "admin", database: "eldenring", connectionLimit: 5 });
+require('dotenv').config()
 
-const getAllUsers = (req, res) => {
-    res.json({ message: 'List of users' });
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    connectionLimit: 5
+});
+
+const getAllUsers = async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const response = await conn.query(
+            "SELECT * FROM users",
+        );
+        //console.log(response);
+        res.json({ response });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred");
+    } finally {
+        if (conn) conn.release();
+    }
 };
 
 const getUserById = (req, res) => {
@@ -11,20 +32,22 @@ const getUserById = (req, res) => {
 };
 
 const createUser = async (req, res) => {
+    const { username, email, password } = req.body;
+    let currentDate = new Date();
     let conn;
     try {
         conn = await pool.getConnection();
         const response = await conn.query(
             "INSERT INTO users (username, password, email, creationDate, modificationDate, creationUser, modificationUser, active) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-            ["username123", "p@ssw0rd", "user123@example.com", new Date(), new Date(), "admin", "admin", true]
+            [username, password, email, currentDate, currentDate, "admin", "admin", true]
         );
-        console.log(response);
+        //console.log(response);
         res.send("User created");
     } catch (error) {
         console.error(error);
         res.status(500).send("An error occurred");
     } finally {
-        if (conn) conn.release(); //release to pool
+        if (conn) conn.release();
     }
 };
 
